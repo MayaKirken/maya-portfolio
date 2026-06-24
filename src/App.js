46a8026d2853e72a8f986f42f2f0888ff4dc9bed
Maya <twebgrl56@gmail.com>
2026-06-24 19:32:09 -0400

    Add navbar styling and layout wiring

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Navbar from './components/Navbar';
 import './styles/App.css';
 
 function App () {
   return (
     <div className="portfolio-app">
+      <Navbar />
       <h1>Maya's Portfolio</h1>
       <p>Blank canvas ready to build.</p>
     </div>
