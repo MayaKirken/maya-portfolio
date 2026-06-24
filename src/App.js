e3b4eea598898bb9e77549d8780d53b1e03ad4f5
Maya <twebgrl56@gmail.com>
2026-06-24 18:05:53 -0400

    Write JSX for nav bar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="portfolio-app">
+      <h1>Maya's Portfolio</h1>
+      <p>Blank canvas ready to build.</p>
     </div>
   );
 }
