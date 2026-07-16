8f0a9b3ec7749775974dfbf70a9a556d5dca8cd2
Maya <twebgrl56@gmail.com>
2026-07-16 16:56:15 -0400

    fix mobile navbar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Hero from './components/Hero';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import './styles/App.css';
+
+
 
 function App () {
   return (
